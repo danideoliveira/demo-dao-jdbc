12a2a46a6cc78f3b80741c4fbf7924584e302710
danideoliveira <daniel.santos.oliveira1981@gmail.com>
2025-07-21 21:25:39 -0300

    insert implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,12 @@
 package application;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -23,5 +25,10 @@ public class Main {
         System.out.println("\n=== TEST 3: seller findAll ===");
         list = sellerDao.findAll();
         System.out.println(list);
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
